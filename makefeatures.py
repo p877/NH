68db5bf925cb034de2eeb03d669f38951146a5d1
p877 <62501932+p877@users.noreply.github.com>
2022-05-02 15:29:01 +0200

    Add files via upload

---
+++ makefeatures.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pyodbc
+
+cnxn = pyodbc.connect('Driver={ODBC Driver 11 for SQL Server};Server=localhost;Database=Full SQL database;Trusted_Connection=yes')
+
+cursor = cnxn.cursor()
+pd.set_option('display.max_columns', None)
+
+def df_quality(df):
+    """
+    Data quality checks per dataframe
+    :param df:
+    :param db_dtypes:
+    :return:
+    """
+
+    def null_count(df):
+        """
+        Percentage of Null values
+        :param df:
+        :return:
+        """
+
+        return df.isna().sum()
+
+    def dtypes(df):
+        """
+        Dataframe data types
+        :param df:
+        :return:
+        """
+        return df.dtypes
+
+    def uniqueness(df):
+        """
+        Dataframe unique values per column
+        :param df:
+        :return:
+        """
+        return df.nunique()
+
+    def df_describe(df):
+        """
+        Dataframe statistics
+        :param df:
+        :return:
+        """
+        return df.describe(include='all')
+
+    df = df.copy()
+    data_describe = df_describe(df)
+    data_types = dtypes(df)
+    data_unique = uniqueness(df)
+
+    data_describe.loc['nulls'] = null_count(df)
+    data_describe.loc['dtypes'] = data_types
+    data_describe.loc['unique'] = data_unique
+
+    return data_describe
+
+
+def temporal_features(df, date_cols):
+    """
+    extracting check-in features
+    :param df: dataframe tat includes booking.checkin_at
+    :return booking_core: dataframe with new columns for extracted features
+            new_cols: list of features label
+    """
+    df[date_cols]=df[date_cols].apply(pd.to_datetime)
+    for col in date_cols:
+        df[col + '_week'] = df[col].dt.isocalendar().week
+        #df[col + '_weekend'] = 0
+
+
+
+    return df
+
+
+# import matplotlib.pyplot as plt
+
+df1=pd.read_sql("select Event,MemberId,JourneyId,ElementType, CAST(Time as DATE) D  from nhMemberEvent",cnxn)
+df2=pd.read_excel('start_end.xlsx',sheet_name='s1',engine='openpyxl')
+df3=pd.read_excel('start_without_end.xlsx',sheet_name='s1',engine='openpyxl')
+df2["target"]=1
+df12=df2
+
+print(df3.dtypes)
+print(df12.dtypes)
+df12["JourneyId"] = df12["JourneyId"].astype("int")
+df3=df3.drop("Time_JourneyCompleted", axis=1)
+df3["target"]=0
+df13=df3
+df123=pd.concat([df12, df13])
+
+
+print(df123.dtypes)
+col_date=["Time_JourneyStarted"]
+features=temporal_features(df123, col_date)
+
+#Remove empty journeyId and empty journey started and journey completed
+features= features[features['JourneyId'].notna()]
+features["Time_JourneyCompleted"].fillna(0, inplace=True)
+features["Time_JourneyStarted"].fillna(0, inplace=True)
+
+
+features.to_csv("featureprocess.csv")
+
+
